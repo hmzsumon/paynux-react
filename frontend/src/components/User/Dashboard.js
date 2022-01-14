2ef196ba7477bd0159caa84ec86fc9d35cabb7c7
Zakaria <zsumonn@gmail.com>
2022-01-15 04:35:25 +0600

    mobile menu added

--- frontend/src/components/User/Dashboard.js
+++ frontend/src/components/User/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAlert } from 'react-alert';
 import { useDispatch, useSelector } from 'react-redux';
 import { DELETE_NOTE_RESET } from '../../constants/noteConstants';
@@ -10,6 +10,7 @@ import {
 	MdOutlineDashboard,
 	MdSend,
 	MdOutlineDescription,
+	MdOutlineMenuOpen,
 } from 'react-icons/md';
 import {
 	AiOutlineDownload,
@@ -81,6 +82,8 @@ const Dashboard = () => {
 	const history = useHistory();
 	const { user } = useSelector((state) => state.user);
 
+	const [isOpen, setIsOpen] = useState(false);
+
 	function logoutUser() {
 		dispatch(logout());
 		alert.success('Logout Successfully');
@@ -102,24 +105,24 @@ const Dashboard = () => {
 	}, [dispatch, isDeleted, deleteError, alert]);
 
 	return (
-		<div>
+		<>
 			{/* <UserDashboardNav /> */}
 			<div className='flex w-full h-screen '>
-				<div className='grid w-full grid-cols-5'>
+				<div className='grid w-full md:grid-cols-5'>
 					{/* Side Nav */}
-					<div className='col-span-1 pt-4 bg-gray-100'>
+					<div className='hidden col-span-1 pt-4 bg-gray-100 md:block '>
 						<div className='flex items-center justify-center '>
 							<Link to='/'>
 								<img src='./pynux_logo.png' alt='logo' className='w-40 ' />
 							</Link>
 						</div>
 						{/* Side Navigation Items */}
-						<div className='mt-16 cursor-pointer'>
+						<div className='mt-16 '>
 							{sideMenuItems.map((item) => {
 								const { id, name, icon } = item;
 								return (
 									<div
-										className='flex items-center w-full px-4 py-4 space-x-4 font-bold text-gray-800 hover:bg-gray-200'
+										className='flex items-center w-full px-4 py-4 space-x-4 font-semibold text-gray-700 transition-all duration-300 ease-in-out cursor-pointer hover:bg-gray-200 hover:text-gray-900'
 										key={id}
 									>
 										<i className='text-2xl'>{icon}</i>
@@ -130,48 +133,88 @@ const Dashboard = () => {
 						</div>
 					</div>
 					{/* Header & Main */}
-					<div className='col-span-4 px-4 pt-4'>
-						<div className='flex items-center justify-end space-x-4'>
-							<div className='flex items-center space-x-2'>
-								<FaUser className='p-1 text-2xl rounded-full text-my_color2 ring-2 ring-my_color1' />
-
-								<p className='inline-flex items-center p-2 text-xl font-medium text-my_color2'>
-									{user.name}
-								</p>
-							</div>
-							<div>
-								<button
-									className='py-2 text-xl font-medium transition-all duration-300 ease-in-out border-2 rounded cursor-pointer hover:text-white border-my_color2 px-9 hover:bg-my_color2 '
-									onClick={() => logoutUser()}
-								>
-									Logout
-								</button>
-							</div>
-						</div>
-						<div className='px-20 mt-20 space-y-10'>
-							{/* Balance */}
-							<div className='space-y-8 '>
-								<h2 className='text-2xl font-semibold text-gray-800'>
-									Balance
-								</h2>
-								<div className='flex items-center justify-between px-4 py-10 border-2 rounded-3xl w-80 border-my_color2'>
-									<h1 className='text-3xl font-bold text-gray-800'>USD</h1>
-									<p className='text-xl text-gray-800'>0</p>
+					<div className='relative flex flex-col items-center w-full md:col-span-4 '>
+						{/* Header */}
+						<div className='flex items-center justify-between w-full bg-white md:justify-end md:bg-transparent'>
+							<div className='md:hidden'>
+								<div className='flex items-center justify-center h-full pl-4 space-x-4 '>
+									<MdOutlineMenuOpen
+										className={`text-3xl rounded-full cursor-pointer md:hidden hover:ring-2 my-transition mobile-menubar ${
+											isOpen ? 'mobile-menubar-active' : ''
+										}`}
+										onClick={() => setIsOpen(!isOpen)}
+									/>
+									<Link to='/'>
+										<img src='./pynux_logo.png' alt='logo' className='w-40 ' />
+									</Link>
 								</div>
 							</div>
+							<div className='flex items-center justify-end p-4 space-x-4 '>
+								<div className='flex items-center space-x-2'>
+									<FaUser className='p-1 text-2xl rounded-full cursor-pointer text-my_color2 ring-2 ring-my_color1' />
 
-							{/* Recent Activity */}
-							<div>
-								<h2 className='text-2xl font-semibold text-gray-800'>
-									Recent Activity
-								</h2>
-								<div></div>
+									<p className='items-center hidden p-2 text-xl font-medium md:inline-flex text-my_color2'>
+										{user.name}
+									</p>
+								</div>
+								<div>
+									<button
+										className='hidden py-2 text-xl font-medium transition-all duration-300 ease-in-out border-2 rounded cursor-pointer md:block hover:text-white border-my_color2 px-9 hover:bg-my_color2 '
+										onClick={() => logoutUser()}
+									>
+										Logout
+									</button>
+								</div>
 							</div>
 						</div>
+						{/* Mobile Menu */}
+						<div
+							className={`absolute inset-y-0 left-0 bg-gray-100 top-14 md:hidden my-transition ${
+								isOpen ? 'active-mobile-menu' : 'mobile-menu'
+							}`}
+						>
+							{sideMenuItems.map((item) => {
+								const { id, name, icon } = item;
+								return (
+									<div
+										className='flex items-center w-full px-4 py-4 space-x-4 font-semibold text-gray-700 cursor-pointer my-transition hover:bg-gray-200 hover:text-gray-900'
+										key={id}
+										onClick={() => setIsOpen(!isOpen)}
+									>
+										<i className='text-2xl'>{icon}</i>
+										<p className='text-xl'>{name}</p>
+									</div>
+								);
+							})}
+						</div>
+						{/* Start Main Section  */}
+						<div className='flex items-center justify-center w-full mt-20 '>
+							<div className='w-full px-4 py-4 space-y-10 md:w-9/12 rounded-3xl '>
+								{/* Balance */}
+								<div className='space-y-8 '>
+									<h2 className='text-2xl font-semibold text-gray-800'>
+										Balance
+									</h2>
+									<div className='flex items-center justify-between px-4 py-10 border-2 rounded-3xl md:w-80 border-my_color2'>
+										<h1 className='text-3xl font-bold text-gray-800'>USD</h1>
+										<p className='text-xl text-gray-800'>0</p>
+									</div>
+								</div>
+
+								{/* Recent Activity */}
+								<div>
+									<h2 className='text-2xl font-semibold text-gray-800'>
+										Recent Activity
+									</h2>
+									<div></div>
+								</div>
+							</div>
+						</div>
+						{/* End Main Section */}
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
